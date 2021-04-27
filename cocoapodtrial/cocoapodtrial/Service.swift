cb50cc1c81a6a2098ac8afe3e594a877170c96a7
santhosh.vel <santhosh.vel@zohocorp.com>
2021-04-27 11:34:10 +0530

    feat framework

---
+++ cocoapodtrial/cocoapodtrial/Service.swift
@@ -0,0 +1,16 @@
+//
+//  Service.swift
+//  cocoapodtrial
+//
+//  Created by Santhosh Kumar on 27/04/21.
+//
+
+import Foundation
+
+
+public class Service{
+    private init(){}
+    public static func doSomething()-> String{
+        return "ohh shit"
+    }
+}
---
+++ cocoapodtrial/cocoapodtrial/cocoapodtrial.h
@@ -0,0 +1,18 @@
+//
+//  cocoapodtrial.h
+//  cocoapodtrial
+//
+//  Created by Santhosh Kumar on 27/04/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for cocoapodtrial.
+FOUNDATION_EXPORT double cocoapodtrialVersionNumber;
+
+//! Project version string for cocoapodtrial.
+FOUNDATION_EXPORT const unsigned char cocoapodtrialVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <cocoapodtrial/PublicHeader.h>
+
+
